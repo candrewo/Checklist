201bd28fa71044f1a96f1107bb95340cfa2cdffa
Andrew <caolegario@gmail.com>
2014-03-17 13:43:51 -0700

    results page

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def home():
  	return render_template('home.html')
 
+@app.route('/results', methods=['POST'])
+def results():
+	item = request.form["item"]
+	amount = request.form["amount"]
+	return render_template('results.html', amount=amount, item=item)
+
 
 if __name__ == '__main__':
   	app.run(debug=True)
