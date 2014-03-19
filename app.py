61fc8df6a00a6713d8d45ac6c91eff1f99b90c01
Andrew <caolegario@gmail.com>
2014-03-18 17:40:04 -0700

    adding items

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template, request
+import json
  
 app = Flask(__name__)      
  
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
- 	return render_template('home.html')
-
-@app.route('/results', methods=['POST'])
-def results():
-	item = request.form["item"]
-	amount = request.form["amount"]
-	return render_template('results.html', amount=amount, item=item)
+ 	items = {'apples': 1, 'oranges':2, 'bananas':3}
+ 	if request.method == 'POST':
+		item = request.form["item"]
+		amount = request.form["amount"]
+		items[item] = amount
+		return render_template('home.html', items=items)
+	else:
+		return render_template('home.html', items=items)
 
 
 if __name__ == '__main__':
