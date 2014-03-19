cdfceab0721594bca264cc4ceec9d1dc0f1aafd3
Andrew <caolegario@gmail.com>
2014-03-19 12:52:10 -0700

    no json

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request
-import json
  
 app = Flask(__name__)      
  
 @app.route('/', methods=['GET', 'POST'])
 def home():
- 	items = {'apples': 1, 'oranges':2, 'bananas':3}
- 	if request.method == 'POST':
-		item = request.form["item"]
-		amount = request.form["amount"]
-		items[item] = amount
-		json.dumps(items)
-		return render_template('home.html', items=items)
-	else:
-		return render_template('home.html', items=items)
+    my_items = {"apples":13.45,"more apples":21.99}
+    new_item = {}
+    if request.method == 'POST':
+        item = request.form["item"]
+        amount = request.form["amount"]
+        my_items[item] = amount
+        return render_template('home.html', my_items=my_items)
+    else:
+        return render_template('home.html', my_items=my_items)
 
 
 if __name__ == '__main__':
-  	app.run(debug=True)
+    app.run(debug=True)
